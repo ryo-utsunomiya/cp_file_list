25bc9020b9ec3d213e31ecb7cd2cb115411d598d
Ryo Utsunomiya <millionsnowliving@gmail.com>
2014-05-15 17:09:22 +0900

    Add dry-run option

--- cp_file_list.php
+++ cp_file_list.php
@@ -2,7 +2,7 @@
 <?php
 
 /**
- * Last modified 2014-03-10
+ * Last modified 2014-05-15
  * @author Ryo Utsunomiya (https://twitter.com/ryo511)
  */
 
@@ -20,17 +20,30 @@ $files = file_get_contents($argv[1]);
 $files = explode("\n", $files);
 $files = array_filter($files, 'strlen');
 
+if (isset($argv[3])) {
+    switch ($argv[3]) {
+        case '--dry-run':
+            $dry_run = true;
+            break;
+        default:
+            $dry_run = false;
+    }
+}
+
 foreach ($files as $filepath) {
     $filepath = trim($filepath);
     if (is_file($filepath) || is_dir($filepath)) {
         $command    = sprintf("cp -a --parents %s %s 2>&1", $filepath, $destination);
-        $output     = array();
-        $return_var = 0;
-        exec($command, $outout, $return_var);
-        // echo $command, PHP_EOL;
-        if ($return_var !== 0) {
-            fputs(STDERR, $output[0]);
-            exit(1);
+        if ($dry_run) {
+            echo $command, PHP_EOL;
+        } else {
+            $output     = array();
+            $return_var = 0;
+            exec($command, $outout, $return_var);
+            if ($return_var !== 0) {
+                fputs(STDERR, $output[0]);
+                exit(1);
+            }
         }
     } else {
         fputs(STDERR, "No such file or directory: $filepath\n");
